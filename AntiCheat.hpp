c5b343a2994f71da3008c7cf764d369b686e966b
ALEX <94417808+AlSch092@users.noreply.github.com>
2022-06-14 13:01:35 -0400

    Update AntiCheat.hpp

--- AntiCheat.hpp
+++ AntiCheat.hpp
@@ -3,7 +3,7 @@
 #include "AntiDebug/AntiDebugger.hpp"
 #include "AntiTamper/Integrity.hpp"
 
-extern "C" __forceinline bool NoStaticAnalysis(); //we want to make a function which is misleading to decompilers, but still decompiles.
+extern "C" __forceinline bool MisleadingFunction(); 
 extern "C" void inline_test(); //using macros within masm file
 
 class AntiCheat //main class of the program, or 'hub'. contains all the detection methods
@@ -13,13 +13,11 @@ public:
 	Process* GetProcessObject() { return this->_Proc; }
 	Debugger::AntiDebug* GetAntiDebugger() { return this->_AntiDebugger; }
 
-	typedef void (*FunctionFunc)();
+	typedef void (*FunctionTypePtr)();
 
-	inline void ShellcodeTests() { //works now
-		
-		//NoStaticAnalysis(); 
-		
-		byte* buffer = (byte*)"\x53\x47\x82\xEB\x07\x47\x8A\x43\x23\x0F\xFE\xDF"; //it would be ideal if some server sent this buffer, thus we could create custom code execution
+	inline void ShellcodeTests() { 
+			
+		byte* buffer = (byte*)"\x53\x47\x82\xEB\x07\x47\x8A\x43\x23\x0F\xFE\xDF";
 
 		DWORD dOldProt = 0;
 		VirtualProtect((LPVOID)buffer, 13, PAGE_EXECUTE_READWRITE, &dOldProt);
@@ -31,13 +29,10 @@ public:
 		foo = (void(*)())(buffer);
 		foo(); //shellcode call
 
-		//VirtualProtect((LPVOID)buffer, 13, PAGE_NOACCESS, &dOldProt);
-
 		///Part 2: virtualalloc + shellcode func call
 
 		LPVOID p = VirtualAlloc(NULL, 13, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-		printf("p:%llx\n", p);
-		
+				
 		if (p != 0)
 		{
 			printf("&P: %llX\n", &p);
@@ -49,16 +44,14 @@ public:
 			foo(); //shellcode call
 			printf("Called p!\n");
 
-			void (*foo2)() = (void(*)())p; //maybe we can call buffer in some sneaky way inside the virtual mem
-			foo2();
-
-			VirtualFree(p, 0, MEM_RELEASE);	 //this will make the memory poof
-
+			//...just to check how compilers treat each of these calls
+			FunctionTypePtr* foo2 = (void(**)())&p; 
+			(*foo2)();  //actually works
+			printf("Called foo2: %llX!\n", *foo2);
+			
+			
+			VirtualFree(p, 0, MEM_RELEASE);	 //memory begone
 		}
-		
-		//we can use virtualalloc to quickly allocate some mem, copy some buffer to it, transform it, then call it and release the mem to help combat attackers as this will only stay in memory for a split-moment
-		//...and we can now make this much more complicated! 
-		
 	}
 
 
