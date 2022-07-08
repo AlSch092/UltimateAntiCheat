2404cd67aaef2a05a46b099c31478475f8eadb07
alex <94417808+AlSch092@users.noreply.github.com>
2022-07-07 20:59:02 -0400

    Update Utility.hpp

--- Utility.hpp
+++ Utility.hpp
@@ -1,36 +1,53 @@
 #pragma once
-#define EXCEPTION_EXECUTE_HANDLER       1
-#define EXCEPTION_CONTINUE_SEARCH       0
-#define EXCEPTION_CONTINUE_EXECUTION    -1
 #include <stdint.h>
-
-template <class T> //prevent needing to redefine on each line
+#include <Windows.h>
 
 class Utility
 {
 public:
 	
+	template<class T>
 	static T ReadPointer(uint64_t ulBase, uint64_t ulOffset)
 	{
 		__try
 		{
 			return *(T*)(*(uint64_t*)ulBase + ulOffset);
 		}
-		__except (EXCEPTION_EXECUTE_HANDLER) { return (T)NULL; }
+		__except (1) { return (T)NULL; }
 	}
 
+	template<class T>
 	static bool WritePointer(uint64_t ulBase, uint64_t ulOffset, T iValue)
 	{
 		__try { *(T*)(*(uint64_t*)ulBase + ulOffset) = iValue; return true; }
-		__except (EXCEPTION_EXECUTE_HANDLER) { return false; }
+		__except (1) { return false; }
 	}
 
+	template<class T>
 	static T DereferenceSafe(uint64_t ulAddress)
 	{
 		__try
 		{
 			return *(T*)ulAddress;
 		}
-		__except (EXCEPTION_EXECUTE_HANDLER) { return (T)NULL; }
+		__except (1) { return (T)NULL; }
 	}
+
+	template<class T>
+	static inline void** GetVTableArray(T* pClass, int* pSize) //thanks to  https://stackoverflow.com/questions/2705927/get-specific-process-memory-space
+	{
+		void** ppVTable = *(void***)pClass;
+
+		if (pSize)
+		{
+			*pSize = 0;
+
+			while (!IsBadReadPtr(ppVTable[*pSize], sizeof(unsigned __int64)))
+				(*pSize)++;
+		}
+
+		return ppVTable;
+	}
+
+	static bool IsVTableHijacked(void* pClass);
 };
