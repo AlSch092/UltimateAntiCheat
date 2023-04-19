5b16ccc45f46413214138673ada67de25033f5aa
Al S <94417808+AlSch092@users.noreply.github.com>
2023-04-19 17:11:49 -0400

    Update main.cpp
    Added TLS callbacks for anti-dll injection

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 // UltimateAnticheat.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // an 'in-development' anti-tamper + anti-debug + anti-load for x86, x64
 // Author: Alsch092,  github: alsch092)
-// Credits to changeofpace for file re-mapping
+// Credits to changeofpace for file re-mapping code
+
+#pragma comment(linker, "/ALIGN:0x10000")
+
+#define DLL_PROCESS_DETACH 0
+#define DLL_PROCESS_ATTACH 1
+#define DLL_THREAD_ATTACH 2
+#define DLL_THREAD_DETACH 3
 
 #include "AntiCheat.hpp"
 #include "Process/Memory/remap.hpp"
@@ -11,11 +18,52 @@
 //  is aligned to the system allocation granularity. At runtime, each section
 //  is padded with pages of reserved memory.
 // NOTE This option does not affect file size.
-#pragma comment(linker, "/ALIGN:0x10000")
+
+
+void NTAPI __stdcall TLSCallback(PVOID DllHandle, DWORD dwReason, PVOID Reserved);
+
+#ifdef _M_IX86
+#pragma comment (linker, "/INCLUDE:__tls_used")
+#pragma comment (linker, "/INCLUDE:__tls_callback")
+#else
+#pragma comment (linker, "/INCLUDE:_tls_used")
+#pragma comment (linker, "/INCLUDE:_tls_callback")
+#endif
+EXTERN_C
+#ifdef _M_X64
+#pragma const_seg (".CRT$XLB")
+const
+#else
+#pragma data_seg (".CRT$XLB")
+#endif
+
+PIMAGE_TLS_CALLBACK _tls_callback = TLSCallback;
+#pragma data_seg ()
+#pragma const_seg ()
 
 using namespace std;
 
-//extern "C" void _MessageBox(); //ignore these!
+void NTAPI __stdcall TLSCallback(PVOID DllHandle, DWORD dwReason, PVOID Reserved)
+{
+    printf("DllHandle: %llX, dwReason: %d, Reserved: %llX\n", DllHandle, dwReason, Reserved);
+
+    switch (dwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        printf("New process attached\n");
+        break;
+
+    case DLL_THREAD_ATTACH:
+        ExitThread(0); //we can stop DLL injecting + DLL debuggers this way
+        printf("New thread spawned!\n");
+        break;
+
+
+    case DLL_THREAD_DETACH:
+        printf("Thread detached!\n");
+        break;
+    };
+}
 
 void GetUserInput()
 {
@@ -36,33 +84,33 @@ void GetUserInput()
 
         switch (userIn[0]) //Todo: finish implementing this!
         {
-            case L'1':
-                cout << "Selected 1.";
-                break;
+        case L'1':
+            cout << "Selected 1.";
+            break;
 
-            case L'2':
-                cout << "Selected 2.";
-                break;
+        case L'2':
+            cout << "Selected 2.";
+            break;
 
-            case L'3':
-                cout << "Selected 3.";
-                break;
+        case L'3':
+            cout << "Selected 3.";
+            break;
 
-            case L'4':
-                cout << "Selected 4.";
-                break;
+        case L'4':
+            cout << "Selected 4.";
+            break;
 
-            case L'5':
-                cout << "Selected 5.";
-                break;
+        case L'5':
+            cout << "Selected 5.";
+            break;
 
-            case L'0':
-                cout << "Done!.";
-                return;
+        case L'0':
+            cout << "Done!.";
+            return;
 
-            default:
-                cout << "Input not recognized as an option. Try again.";
-                break;
+        default:
+            cout << "Input not recognized as an option. Try again.";
+            break;
         }
 
     }
@@ -89,7 +137,7 @@ bool TestProgramHash(AntiCheat* AC)
 
     MessageBoxA(0, "Write '.text' section memory here!", 0, 0);
 
-    if (AC->GetIntegrityChecker()->Check((uint64_t)module, 0x1000, AC->GetProcessObject()->GetModuleHashList())) 
+    if (AC->GetIntegrityChecker()->Check((uint64_t)module, 0x1000, AC->GetProcessObject()->GetModuleHashList()))
     {
         printf("Hashes match! Program appears genuine! Remember to put this inside a TLS callback (and then make sure TLS callback isn't hooked) to ensure we get hashes before memory is tampered.\n");
     }
@@ -148,7 +196,7 @@ void TestAllFunctionalities()
     }
 
     ULONG_PTR ImageBase = (ULONG_PTR)GetModuleHandle(NULL);
-   
+
     if (ImageBase)
     {
         if (!RmpRemapImage(ImageBase))
@@ -164,7 +212,6 @@ void TestAllFunctionalities()
 
 int main()
 {
-  //  _MessageBox();
     TestAllFunctionalities();
     GetUserInput();
 }
