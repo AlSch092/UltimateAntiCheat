ae4316167123c5068974eae6d4a43ffec9d309ae
Al S <94417808+AlSch092@users.noreply.github.com>
2023-04-30 01:26:31 -0400

    Update AntiCheat.hpp
    Code cleaned up significantly

--- AntiCheat.hpp
+++ AntiCheat.hpp
@@ -1,9 +1,22 @@
 #pragma once
+// Check windows
+#if _WIN32 || _WIN64
+#if _WIN64
+#define ENVIRONMENT64
+#else
+#define ENVIRONMENT32
+#endif
+#endif
+
+#include <iostream>
 #include "Process/Process.hpp"
 #include "AntiDebug/AntiDebugger.hpp"
 #include "AntiTamper/Integrity.hpp"
+#include "Services.hpp"
+#include "Obfuscation.hpp"
+#include "Process/Memory/remap.hpp"
 
-extern "C" __forceinline bool MisleadingFunction(); 
+extern "C" __forceinline bool MisleadingFunction(); //the goal here is to get the compiler to inline our function (although apparently not possible on x64) which breaks the static analysis of REing tools.
 extern "C" void inline_test(); //using macros within masm file
 
 class AntiCheat //main class of the program, or 'hub'. contains all the detection methods
@@ -15,47 +28,14 @@ public:
 
 	typedef void (*FunctionTypePtr)();
 
-	inline void ShellcodeTests() { 
-			
-		byte* buffer = (byte*)"\x53\x47\x82\xEB\x07\x47\x8A\x43\x23\x0F\xFE\xDF";
-
-		DWORD dOldProt = 0;
-		VirtualProtect((LPVOID)buffer, 13, PAGE_EXECUTE_READWRITE, &dOldProt);
-
-		for (int i = 0; i < 13; i++) //basic transform of bytes
-			buffer[i] = buffer[i] + 1;
-
-		void (*foo)();
-		foo = (void(*)())(buffer);
-		foo(); //shellcode call
-
-		///Part 2: virtualalloc + shellcode func call
-
-		LPVOID p = VirtualAlloc(NULL, 13, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-				
-		if (p != 0)
-		{
-			printf("&P: %llX\n", &p);
-			printf("P: %llX\n", p);
-
-			memcpy(p, buffer, 13);
-
-			foo = (void(*)())(p);
-			foo(); //shellcode call
-			printf("Called p!\n");
-
-			//...just to check how compilers treat each of these calls
-			FunctionTypePtr* foo2 = (void(**)())&p; 
-			(*foo2)();  //actually works
-			printf("Called foo2: %llX!\n", *foo2);
-			
-			
-			VirtualFree(p, 0, MEM_RELEASE);	 //memory begone
-		}
-	}
-
+	inline void ShellcodeTests();
 
 	inline Integrity* GetIntegrityChecker() { return this->integrityChecker; }
+
+	static bool IsVTableHijacked(void* pClass);
+
+	template<class T>
+	static inline void** GetVTableArray(T* pClass, int* pSize);
 
 protected:
 
