c12fa684a0b2663e724a5c444d1326ec72f4005f
Alex S <94417808+AlSch092@users.noreply.github.com>
2024-03-24 20:26:29 -0400

    Add files via upload

---
+++ Common/Utility.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <stdint.h>
+#include <Windows.h>
+
+class Utility
+{
+public:
+	
+	template<class T>
+	static T ReadPointer(uint64_t ulBase, uint64_t ulOffset)
+	{
+		__try
+		{
+			return *(T*)(*(uint64_t*)ulBase + ulOffset);
+		}
+		__except (1) { return (T)NULL; }
+	}
+
+	template<class T>
+	static bool WritePointer(uint64_t ulBase, uint64_t ulOffset, T iValue)
+	{
+		__try { *(T*)(*(uint64_t*)ulBase + ulOffset) = iValue; return true; }
+		__except (1) { return false; }
+	}
+
+	template<class T>
+	static T DereferenceSafe(uint64_t ulAddress)
+	{
+		__try
+		{
+			return *(T*)ulAddress;
+		}
+		__except (1) { return (T)NULL; }
+	}
+
+};
