1a8f32a5bf0c80a4a36399da18b5351f8492ca16
Alex S <94417808+AlSch092@users.noreply.github.com>
2024-03-30 17:14:06 -0400

    Add files via upload

---
+++ Obscure/SymbolicHash.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <Windows.h>
+
+/*
+this class allows us to call winapi or other library symbolic named funcs without actually referencing the function. if you view the assembly instructions here, you'll get something along 'push 0x554043, call RDI' instead of Call CreateThread or whatever function.
+*/
+class SymbolicHash
+{
+public:
+
+	static PDWORD getFunctionAddressByHash(char* library, DWORD hash);
+	static DWORD getHashFromString(char* str);
+
+	static HANDLE CreateThread_Hash(LPSECURITY_ATTRIBUTES   lpThreadAttributes, SIZE_T dwStackSize, LPTHREAD_START_ROUTINE  lpStartAddress, __drv_aliasesMem LPVOID lpParameter, DWORD dwCreationFlags, LPDWORD lpThreadId);
+
+private:
+
+};
