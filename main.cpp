90839ff369dd7cbbc9f6427539c22edc443aaa52
Alex S <94417808+AlSch092@users.noreply.github.com>
2024-04-01 01:15:37 -0400

    added g_SupressingNewThreads for dll injection prevention

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
 /*  UltimateAnticheat.cpp : This file contains the 'main' function. Program execution begins and ends there. main.cpp contains testing of functionality
 
-    U.A.C. is an 'in-development'/educational anti-cheat written in C++ for x64 platforms
+    U.A.C. is an 'in-development'/educational example of anti-cheat techniques written in C++ for x64 platforms
  
     Feature list: 
-    1. Anti-dll injection (multiple methods including authenticode enforcement and mitigation policy)
+    1. Anti-dll injection (multiple methods, including authenticode enforcement and mitigation policy)
     2. Anti-debugging (multiple methods)
-    3. Anti-tamper  (multiple methods including image remapping)
-    4. PEB modification
+    3. Anti-tamper  (multiple methods including image remapping & memory integrity checking)
+    4. PEB modification & spoofing
     5. Server-generated shellcode execution (self-unpacking + containing a key in each message which is required to be sent back, ensuring the shellcode was executed), plus cipher-chaining
     6. Client-server heartbeats, version checking, licensing, APIs
     7. Modification of modules: changing loaded module names, symbol names (exports and imports)
     8. TLS callback for anti-debugging + anti-dll injection and thread management
     9. WINAPI calls via 'symbolic hashes' -> stores a list of pointers such that we can call winapi routines by a numeric hash instead of its symbol name
 
-    There might be bugs or improvements to techniques, please raise a github issue if you'd like something in particular added or fixed
+    There might be bugs, please raise a github issue if you'd like something in particular added or fixed
 
     Author: AlSch092 @ Github.
 
@@ -46,6 +46,8 @@ PIMAGE_TLS_CALLBACK _tls_callback = TLSCallback;
 
 using namespace std;
 
+bool g_SupressingNewThreads = false;
+
 void NTAPI __stdcall TLSCallback(PVOID DllHandle, DWORD dwReason, PVOID Reserved)
 {
     switch (dwReason)
@@ -57,8 +59,8 @@ void NTAPI __stdcall TLSCallback(PVOID DllHandle, DWORD dwReason, PVOID Reserved
         case DLL_THREAD_ATTACH:
             printf("New thread spawned: %d\n", GetCurrentThreadId());
             
-            //if (PreventingThreadCreation)
-            //    ExitThread(0); //we can stop DLL injecting + DLL debuggers (such as VEH debugger) this way, but make sure you're handling your threads carefully
+            if (g_SupressingNewThreads)
+                ExitThread(0); //we can stop DLL injecting + DLL debuggers (such as VEH debugger) this way, but make sure you're handling your threads carefully
             break;
 
         case DLL_THREAD_DETACH:
@@ -66,7 +68,6 @@ void NTAPI __stdcall TLSCallback(PVOID DllHandle, DWORD dwReason, PVOID Reserved
             break;
     };
 }
-
 
 int main(int argc, char** argv)
 {
@@ -81,6 +82,8 @@ int main(int argc, char** argv)
     AntiCheat* AC = new AntiCheat();
 
     API::Dispatch(AC, API::DispatchCode::INITIALIZE); //initialize AC -> right now basic tests are run within this call 
+
+    g_SupressingNewThreads = AC->GetBarrier()->IsPreventingThreadCreation;
 
     printf("All tests have been executed, the program will now loop using its detection methods for one minute. Thanks for your interest in the project!\n\n");
 
