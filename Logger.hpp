3f0a9e101ae158e1889ff3a305bbcd9a270df1d8
Alex S <94417808+AlSch092@users.noreply.github.com>
2024-04-15 02:09:19 -0400

    added `ErrorType` parameter to log and logf

--- Logger.hpp
+++ Logger.hpp
@@ -7,11 +7,19 @@
 #include <cstdarg>
 #include <mutex>
 
+enum LogType
+{
+    Info,
+    Warning,
+    Err,
+    Detection,
+};
+
 class Logger 
 {
 public:
 
-    static void log(const std::string filename, const std::string& message) 
+    static void log(const std::string filename, LogType type, const std::string& message)
     {
         std::ofstream logFile;
 
@@ -24,19 +32,45 @@ public:
 
         if (logFile.is_open()) 
         {
+            std::string msg_with_errorcode;
+
+            switch (type)
+            {
+            case Err:
+                msg_with_errorcode = "[ERROR] ";
+                break;
+            case Detection:
+                msg_with_errorcode = "[DETECTION] ";
+                break;
+            case Info:
+                msg_with_errorcode = "[INFO] ";
+                break;
+            case Warning:
+                msg_with_errorcode = "[WARNING] ";
+                break;
+
+            default:
+                msg_with_errorcode = "[ERROR] ";
+                break;
+            }
+
+            msg_with_errorcode = msg_with_errorcode + message;
+
             std::time_t now = std::time(nullptr);
             char timestamp[64];
             std::strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", std::localtime(&now));
             logFile << "[" << timestamp << "] " << message << std::endl;
+
             if (logFile.fail()) 
             {
-                std::cerr << "Error: Failed to write to log file." << std::endl;
+                std::cerr << "[ERROR] Failed to write to log file." << std::endl;
             }
+
             logFile.flush();
         }
         else 
         {
-            std::cerr << "Error: Log file is not open." << std::endl;
+            std::cerr << "[ERROR] Log file is not open." << std::endl;
         }
 
         printf("%s\n", message.c_str());
@@ -44,18 +78,17 @@ public:
     }
 
     template<typename... Args>
-    static void logf(const char* filename, const char* format, Args... args)
+    static void logf(const char* filename, LogType type, const char* format, Args... args)
     {
-        char buffer[256];
+        char buffer[512] = { 0 };
         int ret = std::snprintf(buffer, sizeof(buffer), format, args...);
         if (ret >= 0 && ret < sizeof(buffer)) 
         {
-            log(filename, std::string(buffer));
+            log(filename, type, std::string(buffer));
         }
         else 
         {
             std::cerr << "Error: Failed to format log message." << std::endl;
         }
     }
-
 };
