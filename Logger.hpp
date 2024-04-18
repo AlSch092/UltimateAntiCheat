1b2625481cf8d89fc5d2d7efa3c1e957e69245e4
Alex S <94417808+AlSch092@users.noreply.github.com>
2024-04-18 01:47:57 -0400

    changed stack vars to dynamic alloc

--- Logger.hpp
+++ Logger.hpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <ctime>
+#include <time.h>
 #include <cstdarg>
 #include <mutex>
 
@@ -22,6 +22,7 @@ public:
     static void log(const std::string filename, LogType type, const std::string& message)
     {
         std::ofstream logFile;
+        std::string msg_with_errorcode;
 
         logFile.open(filename.c_str(), std::ios::out | std::ios::app);
         if (!logFile.is_open()) 
@@ -31,9 +32,7 @@ public:
         }
 
         if (logFile.is_open()) 
-        {
-            std::string msg_with_errorcode;
-
+        {          
             switch (type)
             {
             case Err:
@@ -57,32 +56,39 @@ public:
             msg_with_errorcode = msg_with_errorcode + message;
 
             std::time_t now = std::time(nullptr);
-            char timestamp[64];
+            char* timestamp = new char[64] {0};
             std::strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", std::localtime(&now));
+            
             logFile << "[" << timestamp << "] " << message << std::endl;
 
             if (logFile.fail()) 
             {
-                std::cerr << "[ERROR] Failed to write to log file." << std::endl;
+                std::cerr << "[ERROR] Failed to write to log file @ Logger::log." << std::endl;
             }
 
             logFile.flush();
+            delete[] timestamp;
         }
         else 
         {
-            std::cerr << "[ERROR] Log file is not open." << std::endl;
+            std::cerr << "[ERROR] Log file is not open @ Logger::log." << std::endl;
         }
 
-        printf("%s\n", message.c_str());
+        printf("%s\n", msg_with_errorcode.c_str());
         logFile.close();
     }
 
     template<typename... Args>
     static void logf(const char* filename, LogType type, const char* format, Args... args)
     {
-        char buffer[512] = { 0 };
-        int ret = std::snprintf(buffer, sizeof(buffer), format, args...);
-        if (ret >= 0 && ret < sizeof(buffer)) 
+        if (format == NULL)
+            return;
+
+        const int buffSize = 512;
+        char* buffer = new char[buffSize] {0};
+
+        int ret = std::snprintf(buffer, buffSize, format, args...);
+        if (ret >= 0 && ret < buffSize)
         {
             log(filename, type, std::string(buffer));
         }
@@ -90,5 +96,7 @@ public:
         {
             std::cerr << "Error: Failed to format log message." << std::endl;
         }
+
+        delete[] buffer;
     }
 };
