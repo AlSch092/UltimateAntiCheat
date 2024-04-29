c3a884fb4fe13d98bf57085d06365ab075b9ab0e
Alex S <94417808+AlSch092@users.noreply.github.com>
2024-04-29 02:52:16 -0400

    added more error checking, comments, changed printf's to Logger::log

--- Process/Exports.cpp
+++ Process/Exports.cpp
@@ -1,7 +1,14 @@
 #include "Exports.hpp"
 
+/*
+	ChangeFunctionName - Changes the exported function name of a module loaded in memory. Please see my project `ModifyExports` for more details/original post. This technique can be used to stop dll injection and more.
+	returns true on success
+*/
 bool Exports::ChangeFunctionName(string dllName, string functionName, string newName)
 {
+	if (dllName.size() == 0 || functionName.size() == 0 || newName.size() == 0)
+		return false;
+
 	bool result = false;
 	DWORD oldProt = 0;
 
@@ -15,7 +22,7 @@ bool Exports::ChangeFunctionName(string dllName, string functionName, string new
 	{
 		if (LoadedImage.MappedAddress == 0)
 		{
-			printf("[ERROR] LoadedImage.MappedAddress was 0 at ChangeFunctionName!\n");
+			Logger::logf("UltimateAnticheat.log", Err, "LoadedImage.MappedAddress was 0 at ChangeFunctionName!");
 			return false;
 		}
 
@@ -28,25 +35,38 @@ bool Exports::ChangeFunctionName(string dllName, string functionName, string new
 
 			for (size_t i = 0; i < ImageExportDirectory->NumberOfNames; i++)
 			{
-				//get RVA 
 				sName = (char*)ImageRvaToVa(LoadedImage.FileHeader, LoadedImage.MappedAddress, dNameRVAs[i], NULL);
 
-				if (strcmp(functionName.c_str(), sName.c_str()) == 0)
+				if (strcmp(functionName.c_str(), sName.c_str()) == 0) //todo: make this strcmp_s
 				{
 					UINT64 funcName_Address = (UINT64)GetModuleHandleA(dllName.c_str()) + dNameRVAs[i]; //get address of symbol
 
-					if (!VirtualProtect((LPVOID)funcName_Address, 1024, PAGE_EXECUTE_READWRITE, &oldProt))				
-						printf("VirtualProtect failed: %d\n", GetLastError());				
+					if (!VirtualProtect((LPVOID)funcName_Address, 1024, PAGE_EXECUTE_READWRITE, &oldProt))
+					{
+						Logger::logf("UltimateAnticheat.log", Err, "VirtualProtect failed @ Exports::ChangeFunctionName: %d\n", GetLastError());
+					}
 					else
 					{
 						strcpy_s((char*)funcName_Address, strlen((char*)funcName_Address) + 1, newName.c_str()); //if you're writing more bytes over than the original strings length, you need to expand + shuffle the entire symbol list's memory appropriately and potentially update any offsets.
 						result = true; //successful case
+						goto ending;
 					}
 				}
 			}
 		}
+		else
+		{
+			Logger::logf("UltimateAnticheat.log", Err, "MapAndLoad failed @ Exports::ChangeFunctionName: %d\n", GetLastError());
+			goto ending;
+		}
+	}
+	else
+	{
+		Logger::logf("UltimateAnticheat.log", Err, "ImageExportDirectory was NULL @ Exports::ChangeFunctionName");
+		return false;
 	}
 
+ending:
 	UnMapAndLoad(&LoadedImage);
 	return true;
 }
