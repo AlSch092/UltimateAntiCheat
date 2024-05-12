15a17d9131bd2a56b6f80dd90c29ae53b8954eb3
Alex S <94417808+AlSch092@users.noreply.github.com>
2024-05-12 02:02:05 -0400

    Add files via upload

--- Server/Program.cs
+++ Server/Program.cs
@@ -11,13 +11,26 @@ namespace UACServer
             const string listen_addr = "0.0.0.0";
             const int port = 5445;
 
+            const string current_ver = "v1.0.0";
+
+            Console.Title = "UltimateAntiCheat Server " + current_ver;
+
             AnticheatServer server = new AnticheatServer();
             server.Start(listen_addr, port);
 
-            Logger.Log("UACServer.log", "Press any key to stop the server...");
-            Console.ReadKey();
+            Logger.Log("UACServer.log", "Server initialized: Press the Q key to stop the program...");
 
-            server.Stop();
+            bool listening_input = true;
+
+            while(listening_input)
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.KeyChar == 'q')
+                {
+                    listening_input = false;
+                    server.Stop();
+                }
+            }
 
             Logger.Log("UACServer.log", "Finished listening...");
         }
