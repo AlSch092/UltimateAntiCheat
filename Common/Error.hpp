f6a6c8f63aeaa28be440e4a88d187bff8f0e57bb
Alex S <94417808+AlSch092@users.noreply.github.com>
2024-05-29 18:26:40 -0400

    added `DetectionFlags` enum for client-server comms

--- Common/Error.hpp
+++ Common/Error.hpp
@@ -25,5 +25,13 @@ enum Error
 	GENERIC_FAIL,
 };
 
-
-
+enum DetectionFlags //used in client-server comms to flag cheaters
+{
+	DEBUGGER,
+	PAGE_PROTECTIONS, //re-remapping
+	CODE_INTEGRITY,   //.text section changes
+	OPEN_PROCESS_HANDLES, 
+	UNSIGNED_DRIVERS,
+	INJECTED_ILLEGAL_PROGRAM,
+	EXTERNAL_ILLEGAL_PROGRAM,
+};
