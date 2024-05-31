c6a95983ac18b74a221dfdd6767184073007070e
Alex S <94417808+AlSch092@users.noreply.github.com>
2024-05-31 17:10:38 -0400

    Add files via upload

---
+++ Server/Detections.cs
@@ -0,0 +1,15 @@
+﻿//UltimateAnticheat Server - By AlSch092 @ Github
+
+namespace UACServer
+{
+    enum DetectionFlags //used in client-server comms to flag cheaters
+    {
+        DEBUGGER,
+        PAGE_PROTECTIONS, //re-remapping
+        CODE_INTEGRITY,   //.text section changes
+        OPEN_PROCESS_HANDLES,
+        UNSIGNED_DRIVERS,
+        INJECTED_ILLEGAL_PROGRAM,
+        EXTERNAL_ILLEGAL_PROGRAM,
+    };
+}
