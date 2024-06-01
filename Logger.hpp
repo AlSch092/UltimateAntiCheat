196e9414d4215a2268b5f5f8c9d7a4f4d334b73e
Alex S <94417808+AlSch092@users.noreply.github.com>
2024-06-01 17:52:48 -0400

    fixed routines from 'random' overflow occurances

--- Logger.hpp
+++ Logger.hpp
@@ -19,126 +19,108 @@ class Logger
 {
 public:
 
-    static void log(const std::string filename, LogType type, const std::string& message)
+    static void log(const std::string& filename, LogType type, const std::string& message)
     {
-        std::ofstream logFile;
-        std::string msg_with_errorcode;
-
-        logFile.open(filename.c_str(), std::ios::out | std::ios::app);
-        if (!logFile.is_open()) 
-        {
-            std::cerr << "Error: Could not open log file: " <<  "UltimateAnticheat.log" << std::endl;
-            return;
-        }
-
-        if (logFile.is_open()) 
-        {          
-            switch (type)
-            {
-            case Err:
-                msg_with_errorcode = "[ERROR] ";
-                break;
-            case Detection:
-                msg_with_errorcode = "[DETECTION] ";
-                break;
-            case Info:
-                msg_with_errorcode = "[INFO] ";
-                break;
-            case Warning:
-                msg_with_errorcode = "[WARNING] ";
-                break;
-
-            default:
-                msg_with_errorcode = "[ERROR] ";
-                break;
-            }
-
-            msg_with_errorcode = msg_with_errorcode + message;
-
-            std::time_t now = std::time(nullptr);
-            char timestamp[64];
-            std::strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", std::localtime(&now));
-            
-            logFile << "[" << timestamp << "] " << msg_with_errorcode << std::endl;
-
-            if (logFile.fail()) 
-            {
-                std::cerr << "[ERROR] Failed to write to log file @ Logger::log." << std::endl;
-            }
-
-            logFile.flush();
-        }
-        else 
-        {
-            std::cerr << "[ERROR] Log file is not open @ Logger::log." << std::endl;
-        }
-
-        printf("%s\n", msg_with_errorcode.c_str());
-        logFile.close();
-    }
-
-    static void logw(const std::string filename, LogType type, const std::wstring& message) //wide string log
-    {
-        std::wofstream logFile;
-        std::wstring msg_with_errorcode;
-
-        logFile.open(filename.c_str(), std::ios::out | std::ios::app);
+        std::ofstream logFile(filename, std::ios::out | std::ios::app);
         if (!logFile.is_open())
         {
-            std::cerr << "Error: Could not open log file: " << "UltimateAnticheat.log" << std::endl;
+            std::cerr << "Error: Could not open log file: " << filename << std::endl;
             return;
         }
 
-        if (logFile.is_open())
+        std::string msg_with_errorcode;
+
+        switch (type)
         {
-            switch (type)
-            {
-            case Err:
-                msg_with_errorcode = L"[ERROR] ";
-                break;
-            case Detection:
-                msg_with_errorcode = L"[DETECTION] ";
-                break;
-            case Info:
-                msg_with_errorcode = L"[INFO] ";
-                break;
-            case Warning:
-                msg_with_errorcode = L"[WARNING] ";
-                break;
-
-            default:
-                msg_with_errorcode = L"[ERROR] ";
-                break;
-            }
-
-            msg_with_errorcode = msg_with_errorcode + message;
-
-            std::time_t now = std::time(nullptr);
-            wchar_t timestamp[256] { 0 };
-            std::wcsftime(timestamp, sizeof(timestamp), L"%Y-%m-%d %H:%M:%S", std::localtime(&now));
-
-            logFile << L"[" << timestamp << L"] " << msg_with_errorcode << std::endl;
-
-            if (logFile.fail())
-            {
-                std::cerr << "[ERROR] Failed to write to log file @ Logger::log." << std::endl;
-            }
-
-            logFile.flush();
-        }
-        else
-        {
-            std::cerr << "[ERROR] Log file is not open @ Logger::log." << std::endl;
+        case Err:
+            msg_with_errorcode = "[ERROR] ";
+            break;
+        case Detection:
+            msg_with_errorcode = "[DETECTION] ";
+            break;
+        case Info:
+            msg_with_errorcode = "[INFO] ";
+            break;
+        case Warning:
+            msg_with_errorcode = "[WARNING] ";
+            break;
+        default:
+            msg_with_errorcode = "[ERROR] ";
+            break;
         }
 
-        wprintf(L"%s\n", msg_with_errorcode.c_str());
-        logFile.close();
+        msg_with_errorcode += message;
+
+        std::time_t now = std::time(nullptr);
+        char timestamp[256] = { 0 };
+        std::strftime(timestamp, 256, "%Y-%m-%d %H:%M:%S", std::localtime(&now));
+
+        logFile << "[" << timestamp << "] " << msg_with_errorcode << std::endl;
+
+        if (logFile.fail())
+        {
+            std::cerr << "[ERROR] Failed to write to log file: " << filename << std::endl;
+            return;
+        }
+
+        std::cout << msg_with_errorcode << std::endl; // Output to console
+    }
+
+    static void logw(const std::string& filename, LogType type, const std::wstring& message)
+    {
+        std::wofstream logFile(filename, std::ios::out | std::ios::app);
+
+        if (!logFile.is_open())
+        {
+            std::cerr << "Error: Could not open log file: " << filename << std::endl;
+            return;
+        }
+
+        std::wstring msg_with_errorcode;
+
+        switch (type)
+        {
+        case Err:
+            msg_with_errorcode = L"[ERROR] ";
+            break;
+        case Detection:
+            msg_with_errorcode = L"[DETECTION] ";
+            break;
+        case Info:
+            msg_with_errorcode = L"[INFO] ";
+            break;
+        case Warning:
+            msg_with_errorcode = L"[WARNING] ";
+            break;
+        default:
+            msg_with_errorcode = L"[ERROR] ";
+            break;
+        }
+
+        msg_with_errorcode += message;
+
+        std::time_t now = std::time(nullptr);
+        std::tm localtime;
+        localtime_s(&localtime, &now);
+        wchar_t timestamp[256] = { 0 };
+        std::wcsftime(timestamp, 256, L"%Y-%m-%d %H:%M:%S", &localtime);
+
+        logFile << L"[" << timestamp << L"] " << msg_with_errorcode << std::endl;
+
+        if (logFile.fail())
+        {
+            std::cerr << "[ERROR] Failed to write to log file: " << filename << std::endl;
+            return;
+        }
+
+        logFile.close(); // Close the file after writing
     }
 
     template<typename... Args>
     static void logf(const char* filename, LogType type, const char* format, Args... args)
     {
-        if (format == NULL)
+
+        if (format == NULL || filename == NULL)
             return;
 
         const int buffSize = 1024;
@@ -151,14 +133,14 @@ public:
         }
         else 
         {
-            std::cerr << "Error: Failed to format log message." << std::endl;
+            std::cerr << "[WARNING] Failed to format log message." << std::endl;
         }
     }
 
     template<typename... Args>
     static void logfw(const char* filename, LogType type, const wchar_t* format, Args... args)
     {
-        if (format == NULL)
+        if (format == NULL || filename == NULL)
             return;
 
         const int buffSize = 1024;
@@ -171,7 +153,7 @@ public:
         }
         else
         {
-            std::cerr << "Error: Failed to format log message." << std::endl;
+            std::cerr << "[WARNING] Failed to format log message." << std::endl;
         }
     }
 
