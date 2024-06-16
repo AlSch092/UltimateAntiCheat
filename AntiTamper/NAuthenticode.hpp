df76fc6ae971a49b4db8d83d5691bea5c9d1851d
Alex S <94417808+AlSch092@users.noreply.github.com>
2024-06-16 18:26:27 -0400

    Add files via upload

--- AntiTamper/NAuthenticode.hpp
+++ AntiTamper/NAuthenticode.hpp
@@ -1,21 +1,16 @@
-//-------------------------------------------------------------------
-// Copyright (C) Microsoft.  All rights reserved.
-// Example of verifying the embedded signature of a PE file by using 
-// the WinVerifyTrust function.
-
-#define _UNICODE 1
-#define UNICODE 1
-
+#pragma once
 #include <tchar.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <windows.h>
 #include <Softpub.h>
 #include <wincrypt.h>
 #include <wintrust.h>
+#include "../Logger.hpp"
 
-// Link with the Wintrust.lib file.
 #pragma comment (lib, "wintrust")
+
+#define CATROOT_PATH L"Windows\\System32\\CatRoot" //needs prefix of correct windows-located drive
+#define CATROOT2_PATH L"Windows\\System32\\CatRoot2" //needs prefix of correct windows-located drive
+#define DRIVERSTORE_PATH L"Windows\\System32\\DriverStore\\FileRepository"
 
 namespace Authenticode
 {
