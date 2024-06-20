93a87c520d219e97756741327572d490fb39edc0
Alex S <94417808+AlSch092@users.noreply.github.com>
2024-06-20 15:22:54 -0400

    fixed one-off error in hash checking on modules

--- AntiTamper/Integrity.cpp
+++ AntiTamper/Integrity.cpp
@@ -123,7 +123,8 @@ ModuleHashData* Integrity::GetModuleHash(const wchar_t* moduleName)
 	{
 		if (strcmp(s->name, ".text") == 0)
 		{
-			vector<uint64_t> hashes = GetMemoryHash((uint64_t)(s->address + GetModuleHandleA(modName.c_str())), s->size); //make hashes of .text of module
+			uint64_t sec_addr = (uint64_t)(s->address) + (uint64_t)GetModuleHandleA(modName.c_str());
+			vector<uint64_t> hashes = GetMemoryHash(sec_addr, s->size); //make hashes of .text of module
 
 			ModuleHashData* moduleHashData = new ModuleHashData();
 			int name_len = wcslen(moduleName);
@@ -182,7 +183,7 @@ bool Integrity::IsModuleModified(const wchar_t* moduleName)
 
 			uint64_t* arr2 = currentModuleHash->Hashes.data();
 
-			for (int i = 0; i < size; i++)
+			for (int i = 0; i < size - 1; i++)
 			{
 				if (arr1[i] != arr2[i])
 				{
