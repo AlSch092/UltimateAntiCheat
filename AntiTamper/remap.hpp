35150e3080ab704803dcfccb420d9b2520680a2a
Alex S. <94417808+AlSch092@users.noreply.github.com>
2024-10-12 23:14:19 -0400

    Update remap.hpp
    add extra comments + link to original project

--- AntiTamper/remap.hpp
+++ AntiTamper/remap.hpp
@@ -1,10 +1,12 @@
-//thanks to changeofpace for remapping method
+//thanks to changeofpace for remapping method!
+//Original self-remapping code project can be found at: https://github.com/changeofpace/self-remapping-code
+
 #pragma once
 
 #include <Windows.h>
 #include "../Common/Logger.hpp"
 
-#define EXPECTED_SECTIONS 6
+#define EXPECTED_SECTIONS 6 //for spoofing number of sections at runtime, which can prevent attackers from traversing sections
 
 //=============================================================================
 // Public Interface
