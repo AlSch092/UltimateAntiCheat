a24f8487930cb4161c092fbb7f8467281f331a6b
Alex S. <94417808+AlSch092@users.noreply.github.com>
2024-10-27 00:32:04 -0400

    add class constructor, add `ExecutionAddress`, `OptionalParam` vars

--- Process/Thread.hpp
+++ Process/Thread.hpp
@@ -11,14 +11,45 @@
 class Thread
 {
 public:
+
+	Thread()
+	{
+	}
+
+	Thread(LPTHREAD_START_ROUTINE toExecute, LPVOID lpOptionalParam)
+	{
+		this->handle = CreateThread(0, 0, toExecute, lpOptionalParam, 0, &this->Id);
+
+		if (this->handle == INVALID_HANDLE_VALUE)
+		{
+			Logger::logf("UltimateAnticheat.log", Err, "Failed to create new thread @ Thread::Thread - address %llX", (UINT_PTR)toExecute);
+		}
+
+		this->ExecutionAddress = (UINT_PTR)toExecute;
+		this->OptionalParam = lpOptionalParam;
+	}
+
+	~Thread()
+	{
+		if (this->handle != INVALID_HANDLE_VALUE)
+		{
+			if (!TerminateThread(this->handle, 0))
+			{
+				Logger::logf("UltimateAnticheat.log", Detection, "TerminateThread failed @ ~Thread");
+			}
+		}
+	}
+
 	HANDLE handle = INVALID_HANDLE_VALUE;
 	DWORD Id = 0;
 	DWORD ContextFlags = 0;
 
+	UINT_PTR ExecutionAddress = 0;
+	LPVOID OptionalParam = nullptr;
+
 	bool ShutdownSignalled = false;
 	bool CurrentlyRunning = false;
 
-	static bool IsThreadRunning(HANDLE threadHandle);
+	static bool IsThreadRunning(HANDLE threadHandle); //these could potentially go into Process.hpp/cpp, since we have one Thread class for each thread, thus a static function is not as well suited to be here
 	static bool IsThreadSuspended(HANDLE threadHandle);
 };
-
