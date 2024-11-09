6d5278bdd4b5a11e848908ecb66ac6dc800e8bd6
Alex S. <94417808+AlSch092@users.noreply.github.com>
2024-11-09 00:28:48 -0500

    add `final` to Services class

--- Environment/Services.hpp
+++ Environment/Services.hpp
@@ -40,7 +40,7 @@ struct DeviceW
 enum WindowsVersion
 {									//Major,Minor :
 	Windows2000 = 50,				//5,0
-	WindowsXP   = 51,			    //5,1
+	WindowsXP = 51,			    //5,1
 	WindowsXPProfessionalx64 = 52,	//5,2
 	WindowsVista = 60,				//6,0
 	Windows7 = 61,					//6,1
@@ -49,13 +49,13 @@ enum WindowsVersion
 	Windows10 = 10,					//10
 	Windows11 = 11,					//10  -> build number changes 
 
-	ErrorUnknown  = 0
+	ErrorUnknown = 0
 };
 
 /*
 The Services class deals with keeping track of loaded drivers & services/recurring tasks on the system, along with misc windows functions
 */
-class Services
+class Services final
 {
 public:
 
@@ -110,4 +110,3 @@ private:
 	list<Service*> ServiceList;
 	list <wstring> DriverPaths;
 };
-
