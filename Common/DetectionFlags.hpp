9acfceafc33e027558693df7fab29a65cca5125e
Alex S. <94417808+AlSch092@users.noreply.github.com>
2024-11-29 14:49:33 -0500

    move detectionFlags enum into its own file

---
+++ Common/DetectionFlags.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+enum DetectionFlags //used in client-server comms to flag cheaters. needs to be visible to NetClient thus not inside the Detections class since we don't want to #include Detections.hpp from NetClient.hpp
+{
+	DEBUGGER,
+	PAGE_PROTECTIONS, //re-remapping
+	CODE_INTEGRITY,   //.text section changes
+	DLL_TAMPERING, //hooking or modifying loaded DLLs
+	BAD_IAT, //IAT hooking
+	OPEN_PROCESS_HANDLES,
+	UNSIGNED_DRIVERS,
+	INJECTED_ILLEGAL_PROGRAM,
+	EXTERNAL_ILLEGAL_PROGRAM,
+	REGISTRY_KEY_MODIFICATIONS
+};
--- Common/Error.hpp
+++ Common/Error.hpp
@@ -24,16 +24,3 @@ enum Error
 	BAD_MODULE,
 	GENERIC_FAIL,
 };
-
-enum DetectionFlags //used in client-server comms to flag cheaters. needs to be visible to NetClient thus not inside the Detections class since we don't want to #include Detections.hpp from NetClient.hpp
-{
-	DEBUGGER,
-	PAGE_PROTECTIONS, //re-remapping
-	CODE_INTEGRITY,   //.text section changes
-	DLL_TAMPERING, //hooking or modifying loaded DLLs
-	BAD_IAT, //IAT hooking
-	OPEN_PROCESS_HANDLES,
-	UNSIGNED_DRIVERS,
-	INJECTED_ILLEGAL_PROGRAM,
-	EXTERNAL_ILLEGAL_PROGRAM,
-};
