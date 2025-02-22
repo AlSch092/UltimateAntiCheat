d2f8065f863ef4df78a363c046cc65ddc4e92869
Teo <teogaming666@gmail.com>
2025-02-22 19:33:39 +0100

    Update Detections.cs
    Update Server DetectionFlags to match DetectionFlags on Client

--- Server/Detections.cs
+++ Server/Detections.cs
@@ -4,14 +4,18 @@ namespace UACServer
 {
     enum DetectionFlags //used in client-server comms to flag cheaters
     {
-        DEBUGGER,
-        PAGE_PROTECTIONS, //re-remapping
-        CODE_INTEGRITY,   //.text section changes
-        DLL_TAMPERING, //hooking or modifying loaded DLLs
-        BAD_IAT, //IAT hooking
-        OPEN_PROCESS_HANDLES,
-        UNSIGNED_DRIVERS,
-        INJECTED_ILLEGAL_PROGRAM,
-        EXTERNAL_ILLEGAL_PROGRAM,
+	DEBUGGER,
+	PAGE_PROTECTIONS, //re-remapping
+	CODE_INTEGRITY,   //.text section changes
+	DLL_TAMPERING, //hooking or modifying loaded DLLs
+	BAD_IAT, //IAT hooking
+	OPEN_PROCESS_HANDLES,
+	UNSIGNED_DRIVERS,
+	INJECTED_ILLEGAL_PROGRAM,
+	EXTERNAL_ILLEGAL_PROGRAM,
+	REGISTRY_KEY_MODIFICATIONS,
+	MANUAL_MAPPING,
+	SUSPENDED_THREAD,
+	HYPERVISOR,
     };
 }
